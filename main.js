2ec6541e1cc8558200636d854259c6828c4df327
abhinav <jabhinav041@gmail.com>
2025-05-03 18:21:38 +0530

    added gravity and jump physics

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   width: window.innerWidth,
+  canvas: document.getElementById('gameCanvas'),
   height: window.innerHeight,
   backgroundColor: "#87CEEB",
   scene: {
@@ -19,11 +20,12 @@ const config = {
 const game = new Phaser.Game(config);
 
 let ball;
+let land;
 let cursors;
 let landHeight = 100;
 const ballRadius = 30;
 let canvasHeight,canvasWidth;
-let ballSpeed=5;
+let ballSpeed=200;
 
 function preload() {
   const circleGfx = this.make.graphics({ x: 0, y: 0, add: false });
@@ -39,6 +41,20 @@ function create() {
   const greenTopHeight = 20;
   canvasWidth = this.scale.width;
   canvasHeight = this.scale.height;
+
+  this.physics.world.gravity.y = 3000;
+
+  land = this.physics.add.staticImage(
+    canvasWidth / 2,
+    canvasHeight - landHeight / 2,
+    null
+  );
+
+  land.displayWidth = canvasWidth;
+  land.displayHeight = landHeight;
+  land.setOrigin(0.5);
+  land.refreshBody();
+  land.setVisible(false);
  
 
   graphics.fillStyle(0x8b4513, 1);
@@ -46,10 +62,15 @@ function create() {
 
   graphics.fillStyle(0x228b22, 1);
   graphics.fillRect(0, canvasHeight - landHeight, canvasWidth, greenTopHeight);
+
   ball = this.physics.add.image(canvasWidth / 4, canvasHeight - landHeight - ballRadius, 'redBall');
   ball.setOrigin(0.5);
   ball.setCollideWorldBounds(true);
   ball.setBounce(0.9);
+  ball.setDragX(600);
+  ball.setDamping(true);
+  ball.setDrag(600, 0); 
+  
   ball.setCircle(ballRadius);
   
 
@@ -59,6 +80,8 @@ function create() {
 
   this.cameras.main.setBounds(0, 0, canvasWidth * 2, canvasHeight);
   this.physics.world.setBounds(0, 0, canvasWidth * 2, canvasHeight);
+
+  this.physics.add.collider(ball, land);
 
   this.add.text(20, 20, "Balls Land", { font: "20px Arial", fill: "#ffffff" });
 }
@@ -70,8 +93,11 @@ function update() {
     }
     else if (cursors.right.isDown) {
         ball.setVelocityX(ballSpeed); 
+    }else if(cursors.up.isDown && ball.body.blocked.down){
+        ball.setVelocityY((-50)*ballSpeed);
     }
     else {
         ball.setVelocityX(0); 
+        ball.setVelocityY(0);
     }
 }
